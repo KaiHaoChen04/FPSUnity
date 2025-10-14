2baf91193f98a94e5407d96ecacc84af0f56e20f
kai <chenkenny414@gmail.com>
2025-10-13 21:31:58 -0400

    InputManager script setup

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
